cfe545cad53b835ddd24285c50656e33f8d67209
Usuário <Usurio@HEITOR>
2024-10-27 16:16:00 -0300

    Adicionar arquivos de projeto.

---
+++ Opositonn/TelaLuta.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Opositonn
+{
+    partial class TelaLuta
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaudeUsuario = new System.Windows.Forms.Label();
+            this.lblSaudeOpositor = new System.Windows.Forms.Label();
+            this.btnInvestir = new System.Windows.Forms.Button();
+            this.btnCanalizar = new System.Windows.Forms.Button();
+            this.lblPoder = new System.Windows.Forms.Label();
+            this.btnMedicar = new System.Windows.Forms.Button();
+            this.btnFlagelar = new System.Windows.Forms.Button();
+            this.btnEngajar = new System.Windows.Forms.Button();
+            this.btnProteger = new System.Windows.Forms.Button();
+            this.btnPerfurar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaudeUsuario
+            // 
+            this.lblSaudeUsuario.AutoSize = true;
+            this.lblSaudeUsuario.Location = new System.Drawing.Point(358, 228);
+            this.lblSaudeUsuario.Name = "lblSaudeUsuario";
+            this.lblSaudeUsuario.Size = new System.Drawing.Size(33, 19);
+            this.lblSaudeUsuario.TabIndex = 0;
+            this.lblSaudeUsuario.Text = "200";
+            // 
+            // lblSaudeOpositor
+            // 
+            this.lblSaudeOpositor.AutoSize = true;
+            this.lblSaudeOpositor.Location = new System.Drawing.Point(358, 106);
+            this.lblSaudeOpositor.Name = "lblSaudeOpositor";
+            this.lblSaudeOpositor.Size = new System.Drawing.Size(33, 19);
+            this.lblSaudeOpositor.TabIndex = 0;
+            this.lblSaudeOpositor.Text = "200";
+            // 
+            // btnInvestir
+            // 
+            this.btnInvestir.Location = new System.Drawing.Point(21, 338);
+            this.btnInvestir.Name = "btnInvestir";
+            this.btnInvestir.Size = new System.Drawing.Size(182, 61);
+            this.btnInvestir.TabIndex = 1;
+            this.btnInvestir.Text = "Investir";
+            this.btnInvestir.UseVisualStyleBackColor = true;
+            this.btnInvestir.Click += new System.EventHandler(this.btnInvestir_Click);
+            // 
+            // btnCanalizar
+            // 
+            this.btnCanalizar.Location = new System.Drawing.Point(209, 338);
+            this.btnCanalizar.Name = "btnCanalizar";
+            this.btnCanalizar.Size = new System.Drawing.Size(182, 61);
+            this.btnCanalizar.TabIndex = 1;
+            this.btnCanalizar.Text = "Canalizar";
+            this.btnCanalizar.UseVisualStyleBackColor = true;
+            this.btnCanalizar.Click += new System.EventHandler(this.btnCanalizar_Click);
+            // 
+            // lblPoder
+            // 
+            this.lblPoder.AutoSize = true;
+            this.lblPoder.Location = new System.Drawing.Point(415, 228);
+            this.lblPoder.Name = "lblPoder";
+            this.lblPoder.Size = new System.Drawing.Size(17, 19);
+            this.lblPoder.TabIndex = 2;
+            this.lblPoder.Text = "0";
+            // 
+            // btnMedicar
+            // 
+            this.btnMedicar.Location = new System.Drawing.Point(397, 338);
+            this.btnMedicar.Name = "btnMedicar";
+            this.btnMedicar.Size = new System.Drawing.Size(182, 61);
+            this.btnMedicar.TabIndex = 1;
+            this.btnMedicar.Text = "Medicar";
+            this.btnMedicar.UseVisualStyleBackColor = true;
+            this.btnMedicar.Click += new System.EventHandler(this.btnMedicar_Click);
+            // 
+            // btnFlagelar
+            // 
+            this.btnFlagelar.Location = new System.Drawing.Point(585, 338);
+            this.btnFlagelar.Name = "btnFlagelar";
+            this.btnFlagelar.Size = new System.Drawing.Size(182, 61);
+            this.btnFlagelar.TabIndex = 1;
+            this.btnFlagelar.Text = "Flagelar";
+            this.btnFlagelar.UseVisualStyleBackColor = true;
+            this.btnFlagelar.Click += new System.EventHandler(this.btnFlagelar_Click);
+            // 
+            // btnEngajar
+            // 
+            this.btnEngajar.Location = new System.Drawing.Point(21, 405);
+            this.btnEngajar.Name = "btnEngajar";
+            this.btnEngajar.Size = new System.Drawing.Size(182, 61);
+            this.btnEngajar.TabIndex = 3;
+            this.btnEngajar.Text = "Engajar";
+            this.btnEngajar.UseVisualStyleBackColor = true;
+            this.btnEngajar.Click += new System.EventHandler(this.btnEngajar_Click);
+            // 
+            // btnProteger
+            // 
+            this.btnProteger.Location = new System.Drawing.Point(209, 405);
+            this.btnProteger.Name = "btnProteger";
+            this.btnProteger.Size = new System.Drawing.Size(182, 61);
+            this.btnProteger.TabIndex = 1;
+            this.btnProteger.Text = "Proteger";
+            this.btnProteger.UseVisualStyleBackColor = true;
+            this.btnProteger.Click += new System.EventHandler(this.btnProteger_Click);
+            // 
+            // btnPerfurar
+            // 
+            this.btnPerfurar.Location = new System.Drawing.Point(397, 405);
+            this.btnPerfurar.Name = "btnPerfurar";
+            this.btnPerfurar.Size = new System.Drawing.Size(182, 61);
+            this.btnPerfurar.TabIndex = 1;
+            this.btnPerfurar.Text = "Perfurar";
+            this.btnPerfurar.UseVisualStyleBackColor = true;
+            this.btnPerfurar.Click += new System.EventHandler(this.btnPerfurar_Click);
+            // 
+            // TelaLuta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 881);
+            this.Controls.Add(this.btnEngajar);
+            this.Controls.Add(this.lblPoder);
+            this.Controls.Add(this.btnFlagelar);
+            this.Controls.Add(this.btnMedicar);
+            this.Controls.Add(this.btnCanalizar);
+            this.Controls.Add(this.btnPerfurar);
+            this.Controls.Add(this.btnProteger);
+            this.Controls.Add(this.btnInvestir);
+            this.Controls.Add(this.lblSaudeOpositor);
+            this.Controls.Add(this.lblSaudeUsuario);
+            this.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "TelaLuta";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TelaLuta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaudeUsuario;
+        private System.Windows.Forms.Label lblSaudeOpositor;
+        private System.Windows.Forms.Button btnInvestir;
+        private System.Windows.Forms.Button btnCanalizar;
+        private System.Windows.Forms.Label lblPoder;
+        private System.Windows.Forms.Button btnMedicar;
+        private System.Windows.Forms.Button btnFlagelar;
+        private System.Windows.Forms.Button btnEngajar;
+        private System.Windows.Forms.Button btnProteger;
+        private System.Windows.Forms.Button btnPerfurar;
+    }
+}
+
---
+++ Opositonn/TelaLuta.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opositonn
+{
+    public partial class TelaLuta : Form
+    {
+        double[] Saude, CoeficientePrecisao;
+        double CoeficienteDano, Poder;
+        bool[] VerificadorEscudo;
+
+        Random rng = new Random();
+
+        public TelaLuta()
+        {
+            InitializeComponent();
+
+            Saude = new double[2];
+            CoeficientePrecisao = new double[2];
+            VerificadorEscudo = new bool[2];
+        }
+
+        private void TelaLuta_Load(object sender, EventArgs e)
+        {
+            Saude[0] = 200;
+            Saude[1] = 200;
+            CoeficientePrecisao[0] = 4;
+            CoeficientePrecisao[1] = 4;
+            VerificadorEscudo[0] = false;
+            VerificadorEscudo[1] = false;
+        }
+
+        private void Analisar()
+        {
+            Saude[0] = Convert.ToDouble(lblSaudeUsuario.Text);
+            Saude[1] = Convert.ToDouble(lblSaudeOpositor.Text);
+            Poder = Convert.ToDouble(lblPoder.Text);
+        }
+
+        private void Atualizar()
+        {
+            lblSaudeUsuario.Text = Saude[0].ToString("0");
+            lblSaudeOpositor.Text = Saude[1].ToString("0");
+            lblPoder.Text = Poder.ToString();
+        }
+
+        private void Investir(int User)
+        {
+            Analisar();
+
+            if (rng.Next(0, 20) < CoeficientePrecisao[User])
+            {
+                MessageBox.Show("Foi por um triz, mas o ataque falhou!", "Errou", MessageBoxButtons.OK);
+                return;
+            }
+
+            CoeficienteDano = 16;
+
+            if (VerificadorEscudo[1 - User]) CoeficienteDano = Math.Max(0, CoeficienteDano - 10);
+
+            Saude[1 - User] = Math.Max(0, Saude[1 - User] - CoeficienteDano);
+
+            Atualizar();
+        }
+
+        private void Canalizar()
+        {
+            Analisar();
+
+            Poder = Math.Min(3, Poder + 1);
+
+            Atualizar();
+        }
+
+        private void Medicar(int User)
+        {
+            Analisar();
+
+            Saude[User] = Math.Min(100, Saude[User] + 40);
+
+            if (User == 0) Poder = Math.Max(0, Poder - 2);
+
+            Atualizar();
+        }
+
+        private void Flagelar(int User)
+        {
+            Analisar();
+
+            if (rng.Next(0, 10) < CoeficientePrecisao[User])
+            {
+                MessageBox.Show("Foi por um triz, mas o ataque falhou!", "Errou", MessageBoxButtons.OK);
+                return;
+            }
+
+            CoeficienteDano = 60;
+
+            if (VerificadorEscudo[1 - User]) CoeficienteDano = Math.Max(0, CoeficienteDano - 40);
+
+            Saude[1 - User] = Math.Max(0, Saude[1 - User] - CoeficienteDano);
+
+            if (User == 0) Poder = Math.Max(0, Poder - 3);
+
+            Atualizar();
+        }
+
+        private void Engajar(int User)
+        {
+            Analisar();
+
+            CoeficientePrecisao[User] = Math.Max(0, CoeficientePrecisao[User] - 4);
+
+            if (User == 0) Poder = Math.Max(0, Poder - 1);
+
+            Atualizar();
+        }
+
+        private void Proteger(int User)
+        {
+            Analisar();
+
+            VerificadorEscudo[User] = true;
+
+            if (User == 0) Poder = Math.Max(0, Poder - 1);
+
+            Atualizar();
+        }
+
+        private void Perfurar(int User)
+        {
+            Analisar();
+
+            CoeficienteDano = 28;
+
+            if (VerificadorEscudo[1 - User]) CoeficienteDano += 12;
+
+            Saude[1 - User] = Math.Max(0, Saude[1 - User] - CoeficienteDano);
+
+            if (User == 0) Poder = Math.Max(0, Poder - 1);
+
+            Atualizar();
+        }
+
+        private async void btnInvestir_Click(object sender, EventArgs e)
+        {
+            if (rng.Next(0, 2) == 0)
+            {
+                Investir(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Investir(0);
+            }
+        }
+
+        private async void btnCanalizar_Click(object sender, EventArgs e)
+        {
+            if (Poder == 3) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Canalizar();
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Canalizar();
+            }
+        }
+
+        private async void btnMedicar_Click(object sender, EventArgs e)
+        {
+            if (Poder < 2) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Medicar(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Medicar(0);
+            }
+        }
+
+        private async void btnFlagelar_Click(object sender, EventArgs e)
+        {
+            if (Poder < 3) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Flagelar(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Flagelar(0);
+            }
+        }
+
+        private async void btnEngajar_Click(object sender, EventArgs e)
+        {
+            if (Poder < 1 || CoeficientePrecisao[0] == 0) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Engajar(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Engajar(0);
+            }
+        }
+
+        private async void btnProteger_Click(object sender, EventArgs e)
+        {
+            if (Poder < 1 || VerificadorEscudo[0]) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Proteger(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Proteger(0);
+            }
+        }
+
+        private async void btnPerfurar_Click(object sender, EventArgs e)
+        {
+            if (Poder < 1 || VerificadorEscudo[0]) return;
+            if (rng.Next(0, 2) == 0)
+            {
+                Perfurar(0);
+                await Task.Delay(1000);
+                Investir(1);
+            }
+            else
+            {
+                Investir(1);
+                await Task.Delay(1000);
+                Perfurar(0);
+            }
+        }
+    }
+}
